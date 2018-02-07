4c622976d3e42f9b9931ba4297b379d3952cb335
Paulius Trucinskas <paulius.trucinskas@student.manchester.ac.uk>
2018-02-07 20:44:43 +0000

    Added basic user functionality

---
+++ BullyShieldWebsite/imports/ui/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import AccountsUIWrapper from './AccountsUIWrapper.js';
+
+// App component - represents the whole app
+export default class App extends Component {
+  render(){
+    return (
+      <AccountsUIWrapper />
+
+    )
+  }
+}
