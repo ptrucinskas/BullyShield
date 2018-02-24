e79ce765860c8245c21d12861a5bb42138e6d66e
Paulius Trucinskas <paulius.trucinskas@student.manchester.ac.uk>
2018-02-24 19:21:07 +0000

    Routing now is tidy, all accessible pages use the same base template

---
+++ src/AccountBundle/Controller/RegistrationController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace AccountBundle\Controller;
+
+use FOS\UserBundle\Event\FilterUserResponseEvent;
+use FOS\UserBundle\Event\FormEvent;
+use FOS\UserBundle\Event\GetResponseUserEvent;
+use FOS\UserBundle\Form\Factory\FactoryInterface;
+use FOS\UserBundle\FOSUserEvents;
+use FOS\UserBundle\Model\UserInterface;
+use FOS\UserBundle\Model\UserManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use FOS\UserBundle\Controller\RegistrationController as BaseController;
+
+class RegistrationController extends BaseController
+{
+    public function confirmedAction()
+    {
+        return $this->redirectToRoute('fos_message_inbox');
+    }
+}
