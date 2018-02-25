e2ec2248c2ab48c2adb4959efc8b3b5da4a70cc7
Paulius Trucinskas <paulius.trucinskas@student.manchester.ac.uk>
2018-02-25 13:31:33 +0000

    New register field for guardian email

--- src/AccountBundle/Entity/User.php
+++ src/AccountBundle/Entity/User.php
@@ -6,6 +6,7 @@ namespace AccountBundle\Entity;
 use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
 use FOS\MessageBundle\Model\ParticipantInterface;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * User
@@ -25,8 +26,36 @@ class User extends BaseUser implements ParticipantInterface
     protected $id;
 
 
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="guardian", type="string", length=180)
+     * @Assert\NotBlank(message="Please enter your guardian's email.", groups={"Registration", "Profile"})
+     */
+    protected $guardian;
 
-    //protected $guardianEmail;
+    /**
+     * Get guardian
+     *
+     * @return string
+     */
+    public function getGuardian()
+    {
+        return $this->guardian;
+    }
+
+    /**
+     * Set guardian
+     *
+     * @param string $guardian
+     *
+     * @return User
+     */
+    public function setGuardian($guardian)
+    {
+        $this->guardian = $guardian;
+        return $this;
+    }
 
     /**
      * Get id
