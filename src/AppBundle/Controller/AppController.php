cf7835add9c4a9717e8ff549e3341677c3e13113
Paulius Trucinskas <paulius.trucinskas@student.manchester.ac.uk>
2018-02-26 19:46:12 +0000

    Refreshing of messages

--- src/AppBundle/Controller/AppController.php
+++ src/AppBundle/Controller/AppController.php
@@ -4,7 +4,12 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Config\Definition\Exception\Exception;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use AccountBundle\Entity\Message;
+use AccountBundle\Entity\Thread;
+use Symfony\Component\Intl\Tests\Data\Provider\Json\JsonRegionDataProviderTest;
 
 class AppController extends Controller
 {
@@ -20,12 +25,23 @@ class AppController extends Controller
      * @Route("/ajax_update_messages", name="ajax_update_messages")
      */
     public function refreshMessageListAction(Request $request){
-
+        $receivedLastUpdate = $request->request->get('last_update');
+        $em = $this->getDoctrine()->getManager();
+        $threadLastUpdated = $this->getDoctrine()
+            ->getRepository(Thread::class)
+            ->find($request->request->get("thread_id"))
+            ->getLastUpdated()
+            ->format("H/i/s/d/m/Y");
+        if($request->request->get('last_update') != $threadLastUpdated){
+            return new JsonResponse("Refresh");
+        }else{
+            return new JsonResponse("Messages up to date");
+        }
 
     }
 
     /**
-     * @Route("/ajax_update_threads", name="ajax_update_messages")
+     * @Route("/ajax_update_threads", name="ajax_update_threads")
      */
     public function refreshThreadListAction(Request $request){
 
