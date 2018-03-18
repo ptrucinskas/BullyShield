dc692a1c922d05ed00ffae9a331e3999be7b5e07
Paulius Trucinskas <paulius.trucinskas@student.manchester.ac.uk>
2018-03-18 01:22:48 +0000

    Revert "Deployment branch with local DB"
    This reverts commit e444d5efc784c174dfa60adcaa2bbe42b2a6a2c9.

--- web/py/HelperClass/MySQLConnect.py
+++ web/py/HelperClass/MySQLConnect.py
@@ -51,9 +51,9 @@ import re
 """
 
 #Connection Settings
-DB_HOST = "localhost"
-DB_USER = "root"
-DB_PSWD = "22224568"
+DB_HOST = "52.23.237.31"
+DB_USER = "bullyshieldadmin"
+DB_PSWD = "csx7"
 DB_NAME = "bullyshield_website"
 
 #global list of black-listed words
